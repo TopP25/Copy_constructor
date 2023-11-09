aa0bf1321bb5fb5da664d1241efd286dc5253997
Igor Putyajkin <putyajkin99@gmail.com>
2023-11-09 18:34:32 +0300

    init 18:34

--- Copy_constructor/Copy.h
+++ Copy_constructor/Copy.h
@@ -6,9 +6,12 @@ class Copy {
 	int denum;
 public:
 	Copy(int num, int denum) : num{ num }, denum { denum } {
-		cout << "Copy constructed for" << this << endl;
+		cout << "Copy constructed for " << this << endl;
 	}
 	Copy() : Copy(1, 1){}
+	Copy(const Copy& copy) : num{ copy.num }, denum{ copy.denum } {
+		cout << "Copy constructed for " << this << endl;
+	}
 	~Copy(){
 		cout << "Copy destructed for " << this << endl;
 	}
